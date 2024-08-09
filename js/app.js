dbe8bd17e0ecce5677c55a250cc3930a6aa90746
Salvatore Rago <salvatorerago373@gmail.com>
2024-08-09 20:16:09 +0200

    Add physics: draft 1

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 import * as THREE from 'three';
 import {Room, PLANES_DEPTH, WALL_HEIGHT} from 'room';
 import DartboardLoader from 'dartboard'
-import DartLoader from 'dart'
+import Dart from 'dart'
+import PhysicalObject from 'physical_object'
+
+// Initialize the list of Mesh objects that will populate the scene
+let sceneMeshes = [];
 
 // Initialize all the needed stuff
 const scene = new THREE.Scene();
@@ -29,46 +33,66 @@ scene.add(ambientLight);
 const dirLight = new THREE.DirectionalLight(0xffffff, 1);
 dirLight.position.set(0, -5, 3 * WALL_HEIGHT / 4);
 dirLight.castShadow = true;
-// dirLight.shadow.mapSize.width = 1024;
-// dirLight.shadow.mapSize.height = 1024;
-// dirLight.shadow.camera.left = -400;
-// dirLight.shadow.camera.right = 350;
-// dirLight.shadow.camera.top = 400;
-// dirLight.shadow.camera.bottom = -300;
-// dirLight.shadow.camera.near = 100;
-// dirLight.shadow.camera.far = 800;
 scene.add(dirLight);
 
 // Add room floor and wall to the scene
 let room = new Room();
 scene.add(room);
+sceneMeshes.push(...room.children);
+for (let c of room.children)
+  c.geometry.computeBoundingBox();
 
 // Load the dartboard
 DartboardLoader.Load().then(obj => {
   scene.add(obj);
+
+  sceneMeshes.push(obj);
+  obj.children[0].geometry.computeBoundingBox();
+
   obj.position.y = room.children[1].position.y - PLANES_DEPTH / 2;
   obj.position.z = WALL_HEIGHT * 3 / 4;
 });
 
 // Load the dart
-DartLoader.Load().then(obj => {
+Dart.Load().then(obj => {
   scene.add(obj);
+
+  sceneMeshes.push(obj);
+  obj.children[0].geometry.computeBoundingBox();
+
   obj.position.z = 1.5;
 });
 
-// Add the cube to the scene
+// Add cubes to the scene
+// Cube 1
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
-cube.position.set(0, 0, 1.5);
+let group1 = new THREE.Group();
+group1.add(cube);
+scene.add(group1);
+sceneMeshes.push(group1);
+group1.children[0].geometry.computeBoundingBox();
+group1.position.set(0, 0, PLANES_DEPTH + 5);
+// Cube 2
+const material2 = new THREE.MeshBasicMaterial({color: 0xfcba03});
+const cube2 = new THREE.Mesh(geometry, material2);
+let group2 = new THREE.Group();
+group2.add(cube2);
+scene.add(group2);
+sceneMeshes.push(group2);
+group2.children[0].geometry.computeBoundingBox();
+group2.position.set(0, 5, PLANES_DEPTH + 5);
+// Add physics
+let physicalCube1 = new PhysicalObject(group1, new THREE.Vector3(0, 0, 0));
+let physicalCube2 = new PhysicalObject(group2, new THREE.Vector3(0, 0, 0));
 
 // Define and add the animation loop
 function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
+
+  physicalCube1.reactToCollision(sceneMeshes);
+  // physicalCube2.reactToCollision(sceneMeshes);
 }
 
 renderer.setAnimationLoop(animate);
---
+++ js/physical_object.js
@@ -0,0 +1,116 @@
+import * as THREE from 'three';
+
+const GRAVITY = -0.02;
+const FRICTION_FACTOR = 0.5;
+
+class CollisionInfo {
+  constructor(isColliding, collisionDirection) {
+    this.isColliding = isColliding;
+    this.collisionDirection = collisionDirection;
+  }
+}
+
+/**
+ * Check for a collision between the object identified by meshObj and the plane of
+ * the room identified by meshPlane.
+ */
+
+/*function roomCollision(meshObj, meshPlane) {
+  const box1 = new THREE.Box3();
+  const box2 = new THREE.Box3();
+  box1.copy(meshObj.geometry.boundingBox).applyMatrix4(meshObj.matrixWorld);
+  box2.copy(meshPlane.geometry.boundingBox).applyMatrix4(meshPlane.matrixWorld);
+  return box1.intersectsPlane(box2);
+}*/
+
+/**
+ * Check for a collision between a PhysicalObject and another object
+ */
+function boxCollision(physObj1, obj2) {
+  const box1 = new THREE.Box3().setFromObject(physObj1.object);
+  const box2 = new THREE.Box3().setFromObject(obj2 instanceof PhysicalObject ? obj2.object : obj2);
+  // const box1 = new THREE.Box3();
+  // const box2 = new THREE.Box3();
+  // box1.copy(mesh1.geometry.boundingBox).applyMatrix4(mesh1.matrixWorld);
+  // box2.copy(mesh2.geometry.boundingBox).applyMatrix4(mesh2.matrixWorld);
+
+  box1.min.add(physObj1.velocity);
+  box1.max.add(physObj1.velocity);
+
+  if(obj2 instanceof PhysicalObject) {
+    box2.min.add(obj2.velocity);
+    box2.max.add(obj2.velocity);
+  }
+
+  let center1 = new THREE.Vector3(0, 0, 0);
+  box1.getCenter(center1);
+  let center2 = new THREE.Vector3(0, 0, 0);
+  box2.getCenter(center2);
+  let centersSub = center1.sub(center2);
+
+  return new CollisionInfo(box1.intersectsBox(box2), centersSub);
+}
+
+export default class PhysicalObject {
+  constructor(object, velocity) {
+    this.setObject(object);
+    this.velocity = velocity; // Vector3
+
+    this.objectMesh.geometry.computeBoundingBox();
+  }
+
+  setObject(object) {
+    this.object = object;
+    this.objectMesh = this.object.children[0];
+  }
+
+  reactToCollision(sceneObjectsMeshes) {
+    this.velocity.z += GRAVITY;
+    let handledCollisionX = false;
+    let handledCollisionY = false;
+    let handledCollisionZ = false;
+
+    for (let i = 0;
+         i < sceneObjectsMeshes.length && (!handledCollisionX || !handledCollisionY || !handledCollisionZ);
+         i++) {
+      let sceneObjMesh = sceneObjectsMeshes[0];
+      let collisionInfo = boxCollision(this, sceneObjMesh);
+
+      if (collisionInfo.isColliding) {
+        if (collisionInfo.collisionDirection.x !== 0 && !handledCollisionX) {
+          handledCollisionX = true;
+          this.velocity.x *= FRICTION_FACTOR;
+          this.velocity.x = -this.velocity.x;
+        }
+        if (collisionInfo.collisionDirection.y !== 0 && !handledCollisionY) {
+          handledCollisionY = true;
+          this.velocity.y *= FRICTION_FACTOR;
+          this.velocity.y = -this.velocity.y;
+        }
+
+        if (collisionInfo.collisionDirection.z !== 0 && !handledCollisionZ) {
+          handledCollisionZ = true;
+          this.velocity.z *= FRICTION_FACTOR;
+          this.velocity.z = -this.velocity.z;
+        }
+      }
+    }
+
+    if (!handledCollisionZ) {
+      this.object.position.z += this.velocity.z;
+    }
+  }
+
+  applyGravity(room) {
+    this.velocity.y += GRAVITY;
+
+    // this is where we hit the ground
+    let collisionInfo = boxCollision()
+    if (
+      roomCollision(this.objectMesh, ground)
+    ) {
+      this.velocity.y *= FRICTION_FACTOR
+      this.velocity.y = -this.velocity.y
+    } else this.position.y += this.velocity.y
+  }
+}
